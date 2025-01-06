f77ea9d1496fa991dc12db3b3e694fffb59fd96b
Boris van Katwijk <bvkatwijk@gmail.com>
2025-01-06 20:12:10 +0100

    -

--- src/test/scala/nl/bvkatwijk/j2html/converter/UnitSpec.scala
+++
@@ -1,8 +0,0 @@
-package nl.bvkatwijk.j2html.converter
-
-import org.scalatest.freespec.AnyFreeSpec
-import org.scalatest.matchers._
-
-abstract class UnitSpec extends AnyFreeSpec with should.Matchers {
-
-}
