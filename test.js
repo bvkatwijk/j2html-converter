834d2ab8cc011b3d888ea4220aec55085e796e12
Boris van Katwijk <bvkatwijk@gmail.com>
2025-01-07 12:52:53 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
+
 import * as index from './index.js';
 
 test("asJava", () => assert.equal(index.asJava('<div></div>'), 'div()'));
