b938c4f87ccf83da2cf2563768c27e8b3dfac1b2
Boris van Katwijk <bvkatwijk@gmail.com>
2025-01-07 12:37:06 +0100

    -

--- test.js
+++ test.js
@@ -5,16 +5,4 @@ import * as index from './index.js';
 
 test("asJava", () => assert.equal(index.asJava('<div></div>'), 'div()'));
 
-test('JSON', () => {
-  const input = {
-    foo: 'hello',
-    bar: 'world'
-  };
-
-  const output = JSON.stringify(input);
-
-  assert.snapshot(output, `{"foo":"hello","bar":"world"}`);
-  assert.equal(JSON.parse(output), input, 'matches original');
-});
-
 test.run();
